3a68498266992727d5dd4f91b29f2268e575ad20
hjkimGithub <rlaguswo3408@gmail.com>
2022-01-03 08:40:17 +0900

    blockchain init

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "Nico",
-    gender: "male",
-    age: 22
-};
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, 
-    you are a ${person.gender}!`;
-};
-console.log(sayHi(person));
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+Block.calculateBloackHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+const genesisBlock = new Block(0, "2093j42op3432", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+const getBlockchain = () => blockchain;
+const getLatestblock = () => blockchain[blockchain.length - 1];
+const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,44 @@
-interface Human {
-  name:string;
-  age:number;
-  gender:string;
+import * as CryptoJS from "crypto-js";
+
+class Block{
+  public index:number;
+  public hash:string;
+  public previousHash:string;
+  public data: string;
+  public timestamp: number;
+
+  static calculateBloackHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => 
+  CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ){
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+
+  }
 }
 
-const person = {
-  name:"Nico",
-  gender:"male",
-  age: 22
-};
+const genesisBlock:Block = new Block(0, "2093j42op3432", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, 
-    you are a ${person.gender}!`;
-  };
-  
-  console.log(sayHi(person));
-  
-  export {};
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestblock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = () : number => Math.round(new Date().getTime()/1000);
+
+export {};
