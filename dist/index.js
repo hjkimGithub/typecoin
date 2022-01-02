42f144203feac2e8d9f730528e4f854356e138d1
hjkimGithub <rlaguswo3408@gmail.com>
2022-01-02 21:49:53 +0900

    practicing typescript

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "Nico",
+    gender: "male",
+    age: 22
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, 
+    you are a ${person.gender}!`;
+};
+console.log(sayHi(person));
+//# sourceMappingURL=index.js.map
